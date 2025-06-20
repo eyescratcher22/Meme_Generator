a6c9b6c6bae2ad2a63c6055343bba8031b2d5701
Amitesh <amiteshsonwani22@gmail.com>
2025-06-20 18:19:17 +0530

    frontend

---
+++ meme_generator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="\src\images\meme.png"></img>
+      <h1>Meme Generator</h1>
+    </header>
+  )
+}
