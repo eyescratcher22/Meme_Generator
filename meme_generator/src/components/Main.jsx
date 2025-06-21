cc1f88ce6984a70b7284297453b8ec495b4e3c71
Amitesh <amiteshsonwani22@gmail.com>
2025-06-21 19:21:37 +0530

    smallchanges

--- meme_generator/src/components/Main.jsx
+++ meme_generator/src/components/Main.jsx
@@ -1,37 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 export default function Main() {
     const [meme, setMeme] = useState({
         topText: "One does not simply",
         bottomText: "Walk into Mordor",
-        randomImage: "http://i.imgflip.com/1bij.jpg"
-    });
-
-    const [allMemeImages, setAllMemeImages] = useState([]);
-
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemes, setAllMemes] = useState([])
+    
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
             .then(res => res.json())
-            .then(data => setAllMemeImages(data.data.memes));
-    }, []);
-
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+    
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: newMemeUrl
+        }))
+    } 
+    
     function handleChange(event) {
-        const { name, value } = event.target;
+        const {value, name} = event.currentTarget
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
-        }));
-    }
-
-    function getRandomMemeImage() {
-        const randomIndex = Math.floor(Math.random() * allMemeImages.length);
-        const url = allMemeImages[randomIndex]?.url;
-        if (url) {
-            setMeme(prevMeme => ({
-                ...prevMeme,
-                randomImage: url
-            }));
-        }
+        }))
     }
 
     return (
@@ -56,17 +53,13 @@ export default function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-
-                <button onClick={getRandomMemeImage}>
-                    Get a new meme image 🖼
-                </button>
+                <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
-
             <div className="meme">
-                <img src={meme.randomImage} alt="Meme" />
-                <h2 className="top">{meme.topText}</h2>
-                <h2 className="bottom">{meme.bottomText}</h2>
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
-    );
+    )
 }
